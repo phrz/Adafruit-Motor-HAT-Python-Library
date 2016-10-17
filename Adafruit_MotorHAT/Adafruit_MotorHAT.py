5bae8d53490de6d506bf46c1a56a0f91570b6ea1
Paul Herz <phrz@me.com>
2016-10-16 19:18:12 -0500

    Fixed indent typo leading to unset properties
    In __init__ of the Adafruit_DCMotor class, three class properties are set at the end of a bunch of conditionals. The code setting these properties was mistakenly indented and thus only part of the `else` block rather than always being run (as was necessary)

--- Adafruit_MotorHAT/Adafruit_MotorHAT.py
+++ Adafruit_MotorHAT/Adafruit_MotorHAT.py
@@ -191,9 +191,9 @@ class Adafruit_DCMotor:
 			in1 = 5
 		else:
 			raise NameError('MotorHAT Motor must be between 1 and 4 inclusive')
-			self.PWMpin = pwm
-			self.IN1pin = in1
-			self.IN2pin = in2
+		self.PWMpin = pwm
+		self.IN1pin = in1
+		self.IN2pin = in2
 
 	def run(self, command):
 		if not self.MC:
